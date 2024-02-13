bf3a095ef96427c2ae2fbf835409f72c38de1719
jasimrazi <jasimrazi17@gmail.com>
2024-02-13 21:47:32 +0530

    Fix pull down to refresh

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -139,166 +139,174 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: RefreshIndicator(
           onRefresh: _fetchBookings,
-          child: ListView.builder(
-            physics: BouncingScrollPhysics(),
-            itemCount: bookings.isEmpty ? 1 : bookings.length + 1,
-            itemBuilder: (context, index) {
-              if (bookings.isEmpty) {
-                // Display a message when there are no bookings to show
-                return Center(
-                  child: Column(
-                    children: [
-                      SizedBox(height: 16),
-                      Text(
-                        'No bookings to show',
-                        style: TextStyle(color: Color(0xffadadad)),
-                      ),
-                    ],
-                  ),
-                );
-              } else if (index < bookings.length) {
-                // Display the booking item
-                var booking = bookings[index].data() as Map<String, dynamic>;
-
-                return Dismissible(
-                  key: Key(bookings[index].id),
-                  confirmDismiss: (DismissDirection direction) async {
-                    bool confirm = await showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text("Confirm"),
-                          content: const Text(
-                            "Are you sure you want to remove this booking?",
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(true),
-                              child: const Text(
-                                "Remove",
-                                style: TextStyle(color: Colors.redAccent),
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(false),
-                              child: const Text("Cancel"),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-
-                    if (confirm) {
-                      try {
-                        await _firestore
-                            .collection('Bookings')
-                            .doc(bookings[index].id)
-                            .delete();
-
-                        // Remove the dismissed item from the list
-                        setState(() {
-                          bookings.removeAt(index);
-                        });
-                      } catch (e) {
-                        print('Error deleting document: $e');
-                      }
-                    }
-
-                    return confirm;
-                  },
-                  background: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.only(right: 20.0),
-                    child: Icon(
-                      Icons.delete_sweep,
-                      color: Colors.white,
-                    ),
-                  ),
-                  child: GestureDetector(
-                    onTap: () {
-                      if (isAdmin) {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AllEvents(
-                              eventId: bookings[index].id,
-                              isAdmin: isAdmin,
-                            ),
-                          ),
-                        );
-                      }
-                    },
-                    child: Container(
-                      margin: EdgeInsets.all(8.0),
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          color: Colors.deepPurple,
-                          width: 1,
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            children: [
+              ListView.builder(
+                physics: AlwaysScrollableScrollPhysics(
+                    parent: BouncingScrollPhysics()),
+                shrinkWrap: true,
+                itemCount: bookings.isEmpty ? 1 : bookings.length + 1,
+                itemBuilder: (context, index) {
+                  if (_auth.currentUser == null) {
+                    // Display a message when no user is signed in
+                    return Center(
+                      child: Column(
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                booking['eventName'] ?? '',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                              Text(
-                                booking['venue'] ?? '',
-                                style: TextStyle(color: Color(0xffadadad)),
-                              ),
-                            ],
+                          SizedBox(height: 16),
+                          Text(
+                            'Please log in to see your bookings',
+                            style: TextStyle(color: Color(0xffadadad)),
                           ),
-                          Container(
-                            width: 100,
-                            padding: EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 12),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: _getStatusBorderColor(booking['state']),
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            child: Text(
-                              booking['state']?.toUpperCase() ?? 'UNKNOWN',
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: _getStatusBorderColor(booking['state']),
-                              ),
-                            ),
-                          )
                         ],
                       ),
-                    ),
-                  ),
-                );
-              } else {
-                // Display the refresh button after the last container
-                return Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Center(
-                    child: isLoading
-                        ? CircularProgressIndicator()
-                        : IconButton(
-                            icon: Icon(
-                              Icons.refresh,
-                              color: Colors.deepPurple,
-                            ),
-                            onPressed: _fetchBookings,
+                    );
+                  } else if (bookings.isEmpty) {
+                    // Display a message when there are no bookings to show
+                    return Center(
+                      child: Column(
+                        children: [
+                          SizedBox(height: 16),
+                          Text(
+                            'No bookings to show',
+                            style: TextStyle(color: Color(0xffadadad)),
                           ),
-                  ),
-                );
-              }
-            },
+                        ],
+                      ),
+                    );
+                  } else if (index < bookings.length) {
+                    // Display the booking item
+                    var booking =
+                        bookings[index].data() as Map<String, dynamic>;
+
+                    return Dismissible(
+                      key: Key(bookings[index].id),
+                      confirmDismiss: (DismissDirection direction) async {
+                        bool confirm = await showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Confirm"),
+                              content: const Text(
+                                "Are you sure you want to remove this booking?",
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(true),
+                                  child: const Text(
+                                    "Remove",
+                                    style: TextStyle(color: Colors.redAccent),
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(false),
+                                  child: const Text("Cancel"),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+
+                        if (confirm) {
+                          try {
+                            await _firestore
+                                .collection('Bookings')
+                                .doc(bookings[index].id)
+                                .delete();
+
+                            // Remove the dismissed item from the list
+                            setState(() {
+                              bookings.removeAt(index);
+                            });
+                          } catch (e) {
+                            print('Error deleting document: $e');
+                          }
+                        }
+
+                        return confirm;
+                      },
+                      background: Container(
+                        color: Colors.red,
+                        alignment: Alignment.centerRight,
+                        padding: EdgeInsets.only(right: 20.0),
+                        child: Icon(
+                          Icons.delete_sweep,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: GestureDetector(
+                        onTap: () {
+                          if (isAdmin) {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AllEvents(
+                                  eventId: bookings[index].id,
+                                  isAdmin: isAdmin,
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(8.0),
+                          padding: EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.deepPurple,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    booking['eventName'] ?? '',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  Text(
+                                    booking['venue'] ?? '',
+                                    style: TextStyle(color: Color(0xffadadad)),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                width: 100,
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 12),
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color:
+                                        _getStatusBorderColor(booking['state']),
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Text(
+                                  booking['state']?.toUpperCase() ?? 'UNKNOWN',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color:
+                                        _getStatusBorderColor(booking['state']),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
